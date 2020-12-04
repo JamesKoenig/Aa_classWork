a9ba680dab5c62d61c2928d8372b702ee07eb442
James Koenig <Koenig.James@Live.com>
2020-12-04 14:32:32 -0800

    saving work to swap diver/navigator roles
    started up polytreenode class and got to the parent= assignment

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,20 @@
 class PolyTreeNode
+  attr_reader :value, :parent, :children
+  def initialize(value)
+    @value    = value
+    @parent   = nil
+    @children = []
+  end
+
+  def parent=(node)
+    return false unless node.is_a?(PolyTreeNode)
+    old_parent = @parent
+    @parent    = node
+
+    node.children << self unless node.children.include?(self)
+
+    old_parent.children.delete(self) if old_parent && old_parent != node
+    return true
+  end
 
 end
