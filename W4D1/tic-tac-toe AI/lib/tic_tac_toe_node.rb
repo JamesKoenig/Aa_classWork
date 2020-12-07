dabbc1f50fad91f21e8e84e2bd53b06fd1d28b7c
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2020-12-07 14:24:27 -0800

    Added logic for winning and losing nodes #winning_node still buggy

--- W4D1/tic-tac-toe AI/lib/tic_tac_toe_node.rb
+++ W4D1/tic-tac-toe AI/lib/tic_tac_toe_node.rb
@@ -1,7 +1,7 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :board, :next_mover_mark
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board           = board
     @next_mover_mark = next_mover_mark
@@ -9,21 +9,46 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    lose_prc = Proc.new { |child| child.losing_node?(evaluator) }
+    if @board.over? && @board.winner == TicTacToeNode.other_mover(evaluator)
+      return true
+    elsif evaluator == @next_mover_mark
+      children.all?(&lose_prc)
+    else
+      children.any?(&lose_prc) 
+    end 
+  end
+
+  def self.other_mover(evaluator)
+    case evaluator
+    when :x
+      :o
+    when :o
+      :x
+    end
   end
 
   def winning_node?(evaluator)
+    win_prc = ->(child) { child.winning_node?(evaluator) }
+    if board.over? && @board.winner == evaluator
+      return true 
+    elsif evaluator == @next_mover_mark
+      children.any?(&win_prc)
+    else
+      children.all?(&win_prc)
+    end
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
     childs = []
-    @board.each_with_index do |row,row_idx|
+    @board.rows.each_with_index do |row,row_idx|
       row.each_with_index do |ele,col_idx|
         if ele == nil
           new_board = @board.dup
-          new_board[row_idx,col_idx] = @next_mover_mark # :x
-          childs << TicTacToeNode(new_board,other_mover_mark,[row_idx,col_idx]) # :o
+          new_board[[row_idx,col_idx]] = @next_mover_mark # :x
+          childs << TicTacToeNode.new(new_board,other_mover_mark,[row_idx,col_idx]) # :o
         end
       end
     end
