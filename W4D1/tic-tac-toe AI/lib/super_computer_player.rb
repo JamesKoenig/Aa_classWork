5c9a9d38d30cf435bc941716e2cbc57b1db89b06
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2020-12-07 15:07:04 -0800

    Added implementation of SuperComputerPlayer class

--- W4D1/tic-tac-toe AI/lib/super_computer_player.rb
+++ W4D1/tic-tac-toe AI/lib/super_computer_player.rb
@@ -2,6 +2,11 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    ttt_n = TicTacToeNode.new(game.board, mark)
+    ttt_n_children = ttt_n.children
+    ttt_n_children.each { |move| return move.prev_move_pos if move.winning_node?(mark) }
+    ttt_n_children.each { |move| return move.prev_move_pos if !move.losing_node?(mark) }
+    raise "CHEATER!" 
   end
 end
 
