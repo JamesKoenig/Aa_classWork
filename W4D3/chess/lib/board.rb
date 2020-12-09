6ce6317c2dcb0f135f65621db2773870371929e0
James Koenig <Koenig.James@Live.com>
2020-12-09 15:02:20 -0800

    Saving work to switch driver-navigator roles
    worked through gaining information via checking UML and what the classes
    we're working with can do

--- W4D3/chess/lib/board.rb
+++ W4D3/chess/lib/board.rb
@@ -9,19 +9,22 @@ class Board
       else 
         Array.new (8) { NullPiece.new() }
       end
-
     end 
   end
 
   def move_piece(start_pos, end_pos)
-    # if there's no piece
-    if false # TODO: placeholder
-      start_pos = 0
+    start_x, star_y = start_pos
+    end_x,   end_y  = end_pos
+
+    # if there's no piece at the starting position
+    if self[start_pos].empty? # is_a?(NullPiece)
+      raise "no piece at this position"
     end
 
     # if the piece cannot move to the location
     if false # TODO: placeholder
-      end_pos = 0
+      # case one
+      raise "can't move to this position"
     end
   end
 end
