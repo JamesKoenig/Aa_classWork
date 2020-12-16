3eeee5225530974bb744a0bc0011c9e4f68de9d4
James Koenig <Koenig.James@Live.com>
2020-12-16 10:13:19 -0800

    committing follow-up practice/work done on sql exercises from last night

--- W5D2/skeleton/lib/04a_countries_bonus.rb
+++ W5D2/skeleton/lib/04a_countries_bonus.rb
@@ -14,6 +14,19 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      gdp > (
+              SELECT
+                MAX(gdp)
+              FROM
+                countries
+              WHERE
+                continent = 'Europe'
+            )
   SQL
 end
 
@@ -21,6 +34,21 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT
+      countries.continent,name,area
+    FROM
+      countries
+      INNER JOIN
+      (
+        SELECT
+          continent,MAX(area) as maxarea
+        FROM
+          countries
+        GROUP BY
+          continent
+      ) AS maxareas
+      ON countries.continent = maxareas.continent
+      AND countries.area = maxareas.maxarea
   SQL
 end
 
