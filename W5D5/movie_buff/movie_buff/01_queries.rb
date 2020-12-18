0920d1d69f18c3c5b6c2c2f96c891fac13184716
James Koenig <Koenig.James@Live.com>
2020-12-18 14:29:41 -0800

    finished up part 1 of ActiveRecord exercises, started 2

--- W5D5/movie_buff/movie_buff/01_queries.rb
+++ W5D5/movie_buff/movie_buff/01_queries.rb
@@ -41,8 +41,12 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
-  
-
+  Movie
+    .select(:id,:title)
+    .joins(:actors)
+    .group('movies.id')
+    .order('COUNT(actors.id) DESC')
+    .limit(3)
 end
 
 def directed_by_one_of(them)
@@ -57,7 +61,10 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-
+  Movie
+    .select(:id,:title)
+    .joins(:director)
+    .where(actors: { name: them })
 end
 
 def movie_names_before_1940
@@ -71,5 +78,7 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie
+    .where("yr < 1940")
+    .pluck(:title)
 end
