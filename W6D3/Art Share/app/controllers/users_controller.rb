4263ce9501d2d8c926dc4ae4384eac0c163b1566
James Koenig <Koenig.James@Live.com>
2020-12-30 15:15:20 -0800

    incorporating stuff from our old project into this

---
+++ W6D3/Art Share/app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update_attributes(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email)
+  end
+end
--- W6D3/Art Share/config/routes.rb
+++ W6D3/Art Share/config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :users
+
+  # 1st argument: URI Pattern (resource path)
+  # to: tells what model it sends it to (Ex. User model)
+  # #show - the method that will be called
+  # as:  You can name your routes by adding an as option (from the Instructions)
+
+  get 'users', to: 'users#index', as: 'users'
+  post 'users', to: 'users#create'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
 end
