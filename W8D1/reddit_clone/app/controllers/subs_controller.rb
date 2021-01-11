2f462073b44d9a39e87ffd3b4a6508cab98c075d
Justin Cheasty <cheasty@gmail.com>
2021-01-11 15:49:32 -0800

    Created Mod/Sub relationship

--- W8D1/reddit_clone/app/controllers/subs_controller.rb
+++ W8D1/reddit_clone/app/controllers/subs_controller.rb
@@ -1,4 +1,6 @@
 class SubsController < ApplicationController
+  before_action :require_logged_in, except: [:index, :show]
+  
   def index
     @subs = Sub.all
     render :index
@@ -7,6 +9,7 @@ class SubsController < ApplicationController
   def create
     @sub = Sub.new(sub_params)
     if @sub.save
+      @sub.add_moderator(current_user)
       redirect_to sub_url(@sub)
     else
       
@@ -14,18 +17,36 @@ class SubsController < ApplicationController
   end
 
   def new
+    @sub = Sub.new
+    render :new
   end
 
   def edit
+    @sub = Sub.find_by(id: params[:id])
+    render :edit
   end
 
   def show
+    @sub = Sub.find_by(id: params[:id])
+    render :show
   end
   
   def update
+    @sub = Sub.find_by(id: params[:id])
+    if @sub
+      @sub.name     = params[:sub][:name]
+      @sub.details  = params[:sub][:details]
+      @sub.save
+      redirect_to sub_url(@sub)
+    end
   end
   
   def destroy
+    @sub = Sub.find_by(id: params[:id])
+    if @sub && current_user == @sub.moderators.first
+      @sub.destroy
+      redirect_to subs_url
+    end
   end
 
   private
