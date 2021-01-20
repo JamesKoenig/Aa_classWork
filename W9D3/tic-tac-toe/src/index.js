3f44d980c8fc1f34c8023822127cbece44c038e4
James Koenig <Koenig.James@Live.com>
2021-01-20 11:55:25 -0800

    included node js files, worked through ttt.css, added basic display logic 🤩

--- W9D3/tic-tac-toe/src/index.js
+++ W9D3/tic-tac-toe/src/index.js
@@ -1,6 +1,10 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js');
+const Game = require('./game.js');
 
   $(() => {
-    // Your code here
+    const game    = new Game();
+    let container =  $('.ttt');
+    const view    = new View(game,container);
+
+    view.setupBoard();
   });
--- W9D3/tic-tac-toe/src/ttt-view.js
+++ W9D3/tic-tac-toe/src/ttt-view.js
@@ -1,11 +1,23 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game  = game;
+    this.board = $el;
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    let $ul = $('<ul></ul>');
+    for(let i = 0; i < 9; i++) {
+      let $li = $('<li></li>');
+      $ul.append($li);
+    }
+    $ul.addClass('board');
+    this.board.append($ul);
+
+  }
 }
 
 module.exports = View;
