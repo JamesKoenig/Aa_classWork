d1fdb7a93c46c5e1787c6550e764beb77ddd48e1
Janette <tinoconjanette@gmail.com>
2021-01-19 16:06:26 -0800

    Finish inheritance exercise

---
+++ W9D2/prototypal inheritance exercises/inherits.js
@@ -0,0 +1,20 @@
+// Function.prototype.inherits = function(parentClass){
+//     const constructor = this.prototype.constructor;
+//     function Surrogate(){}
+//     Surrogate.prototype = parentClass.prototype;
+//     this.prototype = new Surrogate();
+//     this.prototype.constructor = constructor;
+// }
+
+Function.prototype.inherits = function(parentClass) {
+    this.prototype = new Object.create(parentClass).prototype;
+    this.prototype.constructor = this;
+} 
+
+function MovingObject() { }
+
+function Ship() { }
+Ship.inherits(MovingObject);
+
+function Asteroid() { }
+Asteroid.inherits(MovingObject);
