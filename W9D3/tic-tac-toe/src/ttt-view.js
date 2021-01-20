b4646a00106c8d4864565ac3f528f01d79135cdc
James Koenig <Koenig.James@Live.com>
2021-01-20 15:44:14 -0800

    finished up tic tac toe game
    wrapped up ttt-view.js
            removed commented out code
            added missing semicolons
    
    removed random newline in ttt.css
    
    moved webpack script in package.json to be the default 'start' script
    for when npm start is run in the dir

--- W9D3/tic-tac-toe/src/ttt-view.js
+++ W9D3/tic-tac-toe/src/ttt-view.js
@@ -12,16 +12,8 @@ class View {
   bindEvents() {
     $(".board li").click((e) => {
       try {
-        let $h2;
-        let currentPlayer = this.game.currentPlayer;
-        let $box       = $(e.currentTarget);
-        let posInts = $box.data("pos").split(",").map(s => parseInt(s));
-        this.game.playMove(posInts);
-
-        $h2 = $('<h2></h2>');
-        $h2.html(this.game.currentPlayer);
-        $h2.addClass('playerMark');
-        $box.append($h2);
+        this.makeMove($(e.target));
+        this.checkVictory();
       } catch (e) {
         if (e instanceof MoveError) {
           alert(e.msg);
@@ -29,36 +21,39 @@ class View {
           throw e;
         }
       }
-      // debugger
-      // let $box       = $(e.currentTarget);
-      // let posInts = $box.data("pos").split(",").map(s => parseInt(s));
-      // if(this.game.board.isEmptyPos(posInts)) {
-        // let $h2 = $('<h2></h2>');
-        // $h2.html(this.game.currentPlayer);
-        // $h2.addClass('playerMark');
-        // $box.append($h2);
- 
-      //   if(this.game.currentPlayer === "x") {
-      //     $(e.target).css('background-color','red');
-      //   } else {
-      //     $(e.target).css('background-color','blue');
-      //   }
-      //   this.game.playMove(posInts); //e.currentTarget())
-      //   //debugger;
-      // } else {
-      //   alert('Invalid position');
-      // }
     })
   }
 
-  makeMove($square) {}
+  makeMove($square) {
+    let $h2;
+    let currentPlayer = this.game.currentPlayer;
+    let posInts       = $square.data("pos").split(",").map(s => parseInt(s));
+    this.game.playMove(posInts);
+
+    $h2 = $('<h2></h2>');
+    $h2.html(currentPlayer);
+    $h2.addClass('playerMark');
+    $square.append($h2);
+    $square.css('background-color', currentPlayer === "x" ? 'red' : 'blue');
+  }
+
+  checkVictory() {
+    if(this.game.isOver()) {
+      $('.board li').off('click');
+      if(this.game.winner()) {
+        alert(`${this.game.winner()} wins!`);
+      } else {
+        alert('This is a tie!😂');
+      }
+    }
+  }
 
   setupBoard() {
     let $ul = $('<ul></ul>');
     for(let i = 0; i < 3; i++) {
       for(let j = 0; j < 3; j++) {
         let $li = $('<li></li>');
-        $li.data("pos", `${i},${j}`)
+        $li.data("pos", `${i},${j}`);
         $ul.append($li);
       }
     }
