ba729fd575347beee3c47f3826a5a9a86a8e841e
James Koenig <Koenig.James@Live.com>
2021-01-22 17:22:34 -0800

    cleaning up some newlines and trailing whitespace

--- W9D5/Pocket Projects/src/clock.js
+++ W9D5/Pocket Projects/src/clock.js
@@ -4,36 +4,36 @@ class Clock {
     constructor() {
       // 1. Create a Date object.
       const currentTime = new Date();
-  
+
       // 2. Store the hour, minute, and second.
       this.hours = currentTime.getHours();
       this.minutes = currentTime.getMinutes();
       this.seconds = currentTime.getSeconds();
-  
+
       // 3. Call printTime.
       this.printTime();
-  
+
       // 4. Schedule the tick at 1 second intervals.
       setInterval(this._tick.bind(this), 1000);
     }
-  
+
     printTime() {
       // Format the time in HH:MM:SS
       const timeString = [this.hours, this.minutes, this.seconds].join(":");
-  
+
       // Use console.log to print it.
       console.log(timeString);
       return timeString;
     }
-  
+
     _tick() {
       // 1. Increment the time by one second.
       this._incrementSeconds();
-  
+
       // 2. Call printTime.
       this.printTime();
     }
-  
+
     _incrementSeconds() {
       // 1. Increment the time by one second.
       this.seconds += 1;
@@ -42,7 +42,7 @@ class Clock {
         this._incrementMinutes();
       }
     }
-  
+
     _incrementMinutes() {
       this.minutes += 1;
       if (this.minutes === 60) {
@@ -50,18 +50,17 @@ class Clock {
         this._incrementHours();
       }
     }
-  
+
     _incrementHours() {
       this.hours = (this.hours + 1) % 24;
     }
   }
-  
+
 
 const clockDiv = document.getElementById('clock');
 
 Clock.prototype.printTime = function() {
     htmlGenerator(`The Current Time: ${this.printTime()}`, clockDiv);
 }
-
 
 const clock = new Clock();
