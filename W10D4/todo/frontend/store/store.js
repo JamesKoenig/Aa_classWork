9e35276d12ea1d346d522ccd32fc5ba2e4abbb80
annieyxu <annieyxu@berkeley.edu>
2021-01-29 14:54:50 -0800

    wip

--- W10D4/todo/frontend/store/store.js
+++ W10D4/todo/frontend/store/store.js
@@ -1,1 +1,8 @@
-import 
+import {createStore} from 'redux';
+import rootReducer from '../reducers/root_reducer';
+
+const configureStore = () => {
+    return createStore(rootReducer);
+}
+
+export default configureStore;
--- W10D4/todo/frontend/todo_redux.jsx
+++ W10D4/todo/frontend/todo_redux.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+  window.store = configureStore();
   const reactRoot = document.getElementById('react-root');
   ReactDOM.render(<App />, reactRoot);
 });
