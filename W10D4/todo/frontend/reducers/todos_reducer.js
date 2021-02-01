63df6964c873b00ee3d7892b14fd2d61e1ed64bf
James Koenig <Koenig.James@Live.com>
2021-02-01 15:31:08 -0800

    saving redux work to switch roles

--- W10D4/todo/frontend/reducers/todos_reducer.js
+++ W10D4/todo/frontend/reducers/todos_reducer.js
@@ -1,7 +1,35 @@
-import * as Actions from '../actions/todo_actions';
+import { RECEIVE_TODO,
+         RECEIVE_TODOS,
+         REMOVE_TODO } from '../actions/todo_actions';
 
-const todosReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+const todosReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  const nextState = Object.assign({},state);    
+
   switch (action.type) {
+    case RECEIVE_TODOS:
+      return Object.assign({},action.todos);
+    case RECEIVE_TODO:
+      nextState[action.todo.id] = action.todo;
+      return nextState;
+    case REMOVE_TODO:
+      delete nextState[action.todo.id];
+      return nextState;
     default:
       return state;
   }
--- W10D4/todo/frontend/todo_redux.jsx
+++ W10D4/todo/frontend/todo_redux.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import configureStore from './store/store';
+import * as TodoActions from './actions/todo_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   window.store = configureStore(); //only in developing; make sure to remove it later
+  window.TodoActions = TodoActions;
   const reactRoot = document.getElementById('react-root');
   ReactDOM.render(<App />, reactRoot);
 });
