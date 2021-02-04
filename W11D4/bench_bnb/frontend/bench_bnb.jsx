958b6ca7155126a971b3f6ddb1af62f05bb066f5
James Koenig <Koenig.James@Live.com>
2021-02-04 14:37:32 -0800

    added react root controller and view, added react

---
+++ W11D4/bench_bnb/frontend/bench_bnb.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+
+  ReactDOM.render(
+    <p>oh god how did I get here I am not good with computers</p>,
+    root);
+});
