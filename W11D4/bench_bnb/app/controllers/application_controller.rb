a1d40b170f1f32818a063ac863790136de21b57d
James Koenig <Koenig.James@Live.com>
2021-02-04 15:32:52 -0800

    adding backend api views/etc for authentication!

--- W11D4/bench_bnb/app/controllers/application_controller.rb
+++ W11D4/bench_bnb/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+
+  def current_user
+    @current_user = User.find_by(session_token: sessions[:session_token])
+  ends
+
+  def require_logged_in
+    #
+  end
 end
