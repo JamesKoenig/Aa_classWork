1246b26d6ff6cee805b2d655bdb722cea18fb471
timjkim210 <timjkim210@gmail.com>
2021-02-04 15:14:13 -0800

    worked on user model

--- W11D4/bench_bnb/app/models/user.rb
+++ W11D4/bench_bnb/app/models/user.rb
@@ -15,8 +15,28 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, 
                                  allow_null: true }
 
-  before_action  
+  before_action :ensure_session_token
 
-  def  
+  attr_accessor :password
+
+  def ensure_session_token
+    @session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
   end
 end
