14d7ca5cb4546afbc75876c4ac29328921418eb1
James Koenig <Koenig.James@Live.com>
2021-03-02 09:42:41 -0800

    add selection sort solution

--- W15D2/selection_sort_project/lib/selection_sort.js
+++ W15D2/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,24 @@
 function swap(arr, index1, index2) {
+  let holder = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = holder;
+}
 
+function findMinIndex(arr, offset = 0) {
+  let minIndex = offset;
+
+  for(let idx = offset+1; idx < arr.length; idx++) {
+    if(arr[minIndex] > arr[idx])
+      minIndex = idx;
+  }
+
+  return minIndex;
 }
 
 function selectionSort(arr) {
-
+  for(let idx = 0; idx < arr.length; idx++) {
+    swap(arr,idx,findMinIndex(arr,idx));
+  }
 }
 
 module.exports = {
