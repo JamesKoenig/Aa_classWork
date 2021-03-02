0c86337bbf7431a16f8f1229e354dfd87a8c74d7
James Koenig <Koenig.James@Live.com>
2021-03-02 09:57:46 -0800

    add insertion sort solution

--- W15D2/insertion_sort_project/lib/insertion_sort.js
+++ W15D2/insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,13 @@
+//decided to go without subfunctions this time, for whatever reason
 function insertionSort(arr) {
-
+  for(let idx = 0; idx < arr.length; idx++) {
+    for(let jdx = idx; jdx > 0 && arr[jdx] < arr[jdx-1]; jdx--) {
+      //swap the elements
+      let holder = arr[jdx-1];
+      arr[jdx-1] = arr[jdx];
+      arr[jdx] = holder;
+    }
+  }
 }
 
 module.exports = {
