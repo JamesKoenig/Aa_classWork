8e195ca4ec50d2d2134cdeb0236de4f037971fd7
James Koenig <Koenig.James@Live.com>
2021-03-02 09:31:00 -0800

    bubble sort solution

--- W15D2/bubble_sort_project/lib/bubble_sort.js
+++ W15D2/bubble_sort_project/lib/bubble_sort.js
@@ -1,9 +1,21 @@
 function swap(array, idx1, idx2) {
-
+  let holder = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = holder;
+  return array;
 }
 
 function bubbleSort(array) {
-
+  let sorted = false;
+  while(!sorted) {
+    sorted = true;
+    for(let idx = 0; sorted && idx < array.length -1; idx++) {
+      if(array[idx] > array[idx+1]) {
+        swap(array,idx,idx+1);
+        sorted = false;
+      }
+    }
+  }
 }
 
 
